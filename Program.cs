11629de61675953a66d029b657b82c2bdd5f3e35
im4i4a <al.zemskov03@yandex.ru>
2025-10-01 14:49:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_5_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
